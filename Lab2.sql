0835ae66c08ef3db6a3801794dbac589ec1a2673
NikoletaEm <nicolettaemm@gmail.com>
2024-02-04 10:22:55 -0500

    Lab2

---
+++ Lab2.sql
@@ -0,0 +1,21 @@
+create table pop_movies 
+(Init varchar(25),
+Barbie int,
+Wonka int, Poor_things int, Saltburn int, Anyone_but_You int, The_beekeeper int);
+## We insert values into our table
+insert into pop_movies values("G.P",3,null,1,1,null,5),
+("A.P",4,4,null,2,5,null),
+("K.E",5,5,null,null,4,2),
+("N.E",null,null,5,5,1,3),
+("A.P",2,1,4,null,5,null),
+("P.X",4,1,null,3,5,1);
+
+select * from pop_movies;
+
+
+ 
+
+
+
+
+
